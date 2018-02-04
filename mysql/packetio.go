1fd604fce8767cc6f2f054dbd4571380bef615e0
Li Yazhou <me.ssword@gmail.com>
2018-02-04 15:20:43 +0800

    add simple test case with com_quit

--- mysql/packetio.go
+++ mysql/packetio.go
@@ -1,5 +1,7 @@
 package mysql
 
+// https://dev.mysql.com/doc/internals/en/mysql-packet.html
+
 import (
 	"bufio"
 	"fmt"
@@ -8,13 +10,14 @@ import (
 )
 
 const (
-	MAX_PACKET_LEN = 0xFFFFFF // 16MB
+	MAX_PACKET_LENGTH = 1<<24 - 1
+	// MAX_PACKET_PAYLOAD_LENGTH = MAX_PACKET_LENGTH - 4
 )
 
 type PacketIO struct {
 	r        io.Reader
 	w        io.Writer
-	sequence int64
+	sequence uint8
 }
 
 func NewPacketIO(r io.Reader, w io.Writer) *PacketIO {
@@ -26,18 +29,51 @@ func NewPacketIO(r io.Reader, w io.Writer) *PacketIO {
 }
 
 func NewPacketIOByConn(conn net.Conn) *PacketIO {
-	r := bufio.NewReaderSize(conn, 1024)
+	r := bufio.NewReaderSize(conn, 1024*8)
 	w := conn
 	return NewPacketIO(r, w)
 }
 
 func (p *PacketIO) ReadPacket() ([]byte, error) {
-	buf := make([]byte, 512)
-	n, _ := p.r.Read(buf)
+	// [length: byte[3]][sequence_id: byte[1]][playload: byte[length]]
+	header := []byte{0, 0, 0, 0}
 
-	fmt.Printf("%d %s", n, buf[:n])
+	_, err := io.ReadFull(p.r, header)
+	if err != nil {
+		return nil, ErrBadConn
+	}
 
-	return buf[:n], nil
+	length := uint32(header[0]) + uint32(header[1])<<8 + uint32(header[2])<<16
+	if length < 1 {
+		return nil, fmt.Errorf("invalid payload length %d", length)
+	}
+
+	fmt.Printf("payload length: %d", length)
+
+	sequence := uint8(header[3])
+	if p.sequence != sequence {
+		return nil, fmt.Errorf("invalid sequence %d != %d", sequence, p.sequence)
+	}
+
+	p.sequence++
+
+	// TODO: reuse the buffer ?
+	payload := make([]byte, length)
+	_, err = io.ReadFull(p.r, payload)
+	if err != nil {
+		return nil, ErrBadConn
+	}
+
+	// TODO: 确认这里应该使用 MAX_PACKET_PAYLOAD_LENGTH 还是 MAX_PACKET_LENGTH
+	if length < MAX_PACKET_LENGTH {
+		return payload, nil
+	} else {
+		nextPayload, err := p.ReadPacket()
+		if err != nil {
+			return nil, err
+		}
+		return append(payload, nextPayload...), nil
+	}
 }
 
 func (p *PacketIO) WriteErrorPacket(err error) {
--- mysql/packetio_test.go
+++ mysql/packetio_test.go
@@ -13,3 +13,16 @@ func TestNewPacketIO(t *testing.T) {
 
 	mysql.NewPacketIO(r, w)
 }
+
+func TestPacketIOWithComQuit(t *testing.T) {
+	comQuit := []byte{01, 00, 00, 00, 01}
+	buf := bytes.NewBuffer(comQuit)
+	p := mysql.NewPacketIO(buf, buf)
+	payload, err := p.ReadPacket()
+	if err != nil {
+		t.Fatalf("err on p.ReadPacket: %s", err)
+	}
+	if !bytes.Equal(payload, []byte{1}) {
+		t.Fatalf("payload not equal")
+	}
+}
