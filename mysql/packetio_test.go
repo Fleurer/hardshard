ae6650475981194f5b90b97013b638d569f2b3e6
Li Yazhou <me.ssword@gmail.com>
2018-02-04 15:39:11 +0800

    add TestPacketIOWithComQuery1

--- mysql/packetio_test.go
+++ mysql/packetio_test.go
@@ -26,3 +26,26 @@ func TestPacketIOWithComQuit(t *testing.T) {
 		t.Fatalf("payload not equal")
 	}
 }
+
+func TestPacketIOWithComQuery1(t *testing.T) {
+	// test sample from https://dev.mysql.com/doc/internals/en/example-one-mysql-packet.html
+	data := []byte{
+		0x2e, 0x00, 0x00, 0x00, 0x03, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x20, 0x22, 0x30, 0x31, 0x32,
+		0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38,
+		0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34,
+		0x35, 0x22,
+	}
+	buf := bytes.NewBuffer(data)
+	p := mysql.NewPacketIO(buf, buf)
+	payload, err := p.ReadPacket()
+	if err != nil {
+		t.Fatalf("err on p.ReadPacket: %s", err)
+	}
+	if len(payload) != 46 {
+		t.Fatalf("payload len mismatch: %d", len(payload))
+	}
+	expectedPayload := []byte("\x03select \"012345678901234567890123456789012345\"")
+	if !bytes.Equal(payload, expectedPayload) {
+		t.Fatalf("payload not equal\npayload: %b\nexpected: %b", payload, expectedPayload)
+	}
+}
