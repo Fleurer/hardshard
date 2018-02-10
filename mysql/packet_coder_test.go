3d7cf3ddefe80abdfb81bc2e6122c32f9f4b8629
Li Yazhou <me.ssword@gmail.com>
2018-02-10 17:33:00 +0800

    add TestEncodeError2

--- mysql/packet_coder_test.go
+++ mysql/packet_coder_test.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -31,6 +32,19 @@ func TestEncodeError(t *testing.T) {
 		0xff, 0x48, 0x04,
 		0x23, 0x48, 0x59, 0x30, 0x30, 0x30, 0x4e, 0x6f, 0x20,
 		0x74, 0x61, 0x62, 0x6c, 0x65, 0x73, 0x20, 0x75, 0x73, 0x65, 0x64,
+	}
+
+	if !bytes.Equal(payload, expectedPayload) {
+		t.Fatalf("bad result %v, expected: %v", payload, expectedPayload)
+	}
+}
+
+func TestEncodeError2(t *testing.T) {
+	// test example from https://dev.mysql.com/doc/dev/mysql-server/8.0.0/page_protocol_basic_err_packet.html
+	c := setupPacketCoder()
+	payload := c.encodeError(fmt.Errorf("oh fuck"))
+	expectedPayload := []byte{
+		255, 81, 4, 35, 72, 89, 48, 48, 48, 111, 104, 32, 102, 117, 99, 107,
 	}
 
 	if !bytes.Equal(payload, expectedPayload) {
