709b3faf45826c9852c566088c0b45cbc6369e05
Li Yazhou <me.ssword@gmail.com>
2018-02-10 16:49:33 +0800

    check in mysql/error.go from mixer

--- mysql/error.go
+++ mysql/error.go
@@ -1,8 +1,33 @@
 package mysql
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrBadConn       = errors.New("connection was bad")
 	ErrMalformPacket = errors.New("Malform packet error")
 )
+
+type MySqlError struct {
+	Code    uint16
+	Message string
+	State   string
+}
+
+func (e *MySqlError) Error() string {
+	return fmt.Sprintf("MYSQL ERROR %d (%s): %s", e.Code, e.State, e.Message)
+}
+
+func NewError(code uint16, message string) MySqlError {
+	e := MySqlError{}
+	e.Code = code
+	e.Message = message
+	if s, ok := MySQLState[code]; ok {
+		e.State = s
+	} else {
+		e.State = DEFAULT_MYSQL_STATE
+	}
+	return e
+}
