3ee3121c91c7f8049a9df4ed371ef69118c34293
Li Yazhou <me.ssword@gmail.com>
2018-02-10 19:00:20 +0800

    take Uint16ToBytes instead of bit wise operation

--- mysql/packet_coder.go
+++ mysql/packet_coder.go
@@ -4,7 +4,7 @@ type PacketCoder struct {
 	capabilities uint32
 }
 
-func (c *PacketCoder) encodeOK(status uint, affectedRows uint64, insertId uint64) []byte {
+func (c *PacketCoder) encodeOK(status uint16, affectedRows uint64, insertId uint64) []byte {
 	// OK_PACKET: https://dev.mysql.com/doc/dev/mysql-server/8.0.0/page_protocol_basic_ok_packet.html
 	// As of MySQL 5.7.5, OK packes are also used to indicate EOF, and EOF packets are deprecated.
 	// These rules distinguish whether the packet represents OK or EOF:
@@ -17,8 +17,8 @@ func (c *PacketCoder) encodeOK(status uint, affectedRows uint64, insertId uint64
 
 	if c.capabilities&CLIENT_PROTOCOL_41 > 0 {
 		// if CLIENT_PROTOCOL_41 is set, the packet contains a warning count.
-		payload = append(payload, byte(status), byte(status>>8)) // status_flags
-		payload = append(payload, 0, 0)                          // number of warnings
+		payload = append(payload, Uint16ToBytes(status)...) // status_flags
+		payload = append(payload, Uint16ToBytes(0)...)      // number of warnings
 	}
 	return payload
 }
@@ -28,14 +28,15 @@ func (c *PacketCoder) encodeEOF(warnings uint16, statusFlags uint16) []byte {
 	payload := make([]byte, 0, 5)
 	payload = append(payload, EOF_HEADER)
 	if c.capabilities&CLIENT_PROTOCOL_41 > 0 {
-		payload = append(payload, byte(warnings), byte(warnings>>8))       // number of warnings
-		payload = append(payload, byte(statusFlags), byte(statusFlags>>8)) // SERVER_STATUS_flags_enum
+		payload = append(payload, Uint16ToBytes(warnings)...)    // number of warnings
+		payload = append(payload, Uint16ToBytes(statusFlags)...) // SERVER_STATUS_flags_enum
 	}
 	return payload
 }
 
 func (c *PacketCoder) encodeError(e error) []byte {
 	// ERR_PACKET: https://dev.mysql.com/doc/dev/mysql-server/8.0.0/page_protocol_basic_err_packet.html
+	// https://dev.mysql.com/doc/dev/mysql-server/8.0.0/page_protocol_basic_dt_strings.html
 	var m *MySqlError
 	var ok bool
 
@@ -46,14 +47,14 @@ func (c *PacketCoder) encodeError(e error) []byte {
 
 	payload := make([]byte, 0, 16+len(m.Message))
 	payload = append(payload, ERR_HEADER)
-	payload = append(payload, byte(m.Code), byte(m.Code>>8))
+	payload = append(payload, Uint16ToBytes(m.Code)...)
 
 	if c.capabilities&CLIENT_PROTOCOL_41 > 0 {
 		// It contains a SQL state value if CLIENT_PROTOCOL_41 is enabled.
 		payload = append(payload, '#')
-		payload = append(payload, m.State...)
+		payload = append(payload, m.State...) // FixedLengthString, always 5 bytes length
 	}
 
-	payload = append(payload, m.Message...)
+	payload = append(payload, m.Message...) // RestOfPacketString
 	return payload
 }
